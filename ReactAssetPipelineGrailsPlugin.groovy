5165894c82442b33150b2b2096406f98bcca08a7
philipp <philipp@uberall.com>
2015-02-06 20:16:34 +0100

    initial commit

---
+++ ReactAssetPipelineGrailsPlugin.groovy
@@ -0,0 +1,13 @@
+class ReactAssetPipelineGrailsPlugin {
+   def version = "1.0.0"
+    def grailsVersion = "2.2 > *"
+    def title = "React Asset-Pipeline Plugin"
+    def author = "Philipp Eschenbach"
+    def authorEmail = "philipp@errbuddy.net"
+    def description = "Provides react support for Asset-Pipeline."
+    def documentation = "http://github.com/peh/react-grails-asset-pipeline"
+    def license = "APACHE"
+    def organization = [ name: "errbuddy", url: "https://errbuddy.net/" ]
+    def issueManagement = [ system: "GITHUB", url: "http://github.com/peh/react-grails-asset-pipeline/issues" ]
+    def scm = [ url: "http://github.com/peh/react-grails-asset-pipeline" ]
+}
