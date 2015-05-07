6a48bab5025ba40bb2dac282945145bc5556c16e
Tobia Conforto <tobia.conforto@gruppo4.it>
2015-05-07 19:22:28 +0200

    Removed explicit environment test in ReactTagLib

--- grails-app/taglib/reactassetpipeline/ReactTagLib.groovy
+++ grails-app/taglib/reactassetpipeline/ReactTagLib.groovy
@@ -1,20 +1,17 @@
 package reactassetpipeline
 
-import grails.util.Environment
-
 class ReactTagLib {
     static namespace = "asset"
     static defaultEncodeAs = [taglib:'raw']
     def grailsApplication
-    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
 
     def reactTemplate = {attrs ->
         def src = attrs.remove('src')
-        attrs.remove('href')
 
         def conf = grailsApplication.config.grails.assets
         def debugParameter = params."_debugResources" == 'y' || params."_debugAssets" == "y"
-        def debugMode = (conf.allowDebugParam && debugParameter) ||  (Environment.current == Environment.DEVELOPMENT && !grailsApplication.warDeployed && conf.bundle != true)
+        def debugMode = (conf.allowDebugParam && debugParameter) ||
+                        (!grailsApplication.warDeployed && conf.bundle != true)
         if(debugMode)
             src = "${asset.pipeline.AssetHelper.nameWithoutExtension(src)}.jsx"
         else
